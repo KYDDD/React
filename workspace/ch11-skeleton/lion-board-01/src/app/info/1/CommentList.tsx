af7da203d74a73099ff34697b4e772a0dacfffbb
yeono <126737362+KYDDD@users.noreply.github.com>
2025-07-02 01:27:37 +0900

    라이언보드 컴포넌트 분리

---
+++ workspace/ch11-skeleton/lion-board-01/src/app/info/1/CommentList.tsx
@@ -0,0 +1,15 @@
+import CommentItem from "./CommentItem";
+import CommentNew from "./CommentNew";
+
+export default function CommentList() {
+  return (
+    <section className="mb-8">
+      <h4 className="mt-8 mb-4 ml-2">댓글 2개</h4>
+
+      <CommentItem />
+      <CommentItem />
+
+      <CommentNew />
+    </section>
+  );
+}
